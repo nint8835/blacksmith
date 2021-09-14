ea0f60412b3bad32d5726b724d388301c8f0316d
Riley Flynn <riley@rileyflynn.me>
2021-09-13 22:24:58 -0230

    Move from guild to global

--- src/scripts/createCommands.mjs
+++ src/scripts/createCommands.mjs
@@ -5,48 +5,45 @@ import config from '../../config.mjs';
 
 const rest = new REST({ version: '9' }).setToken(config.token);
 
-await rest.put(
-  Routes.applicationGuildCommands(config.appId, config.testingGuildId),
-  {
-    body: [
-      new SlashCommandBuilder()
-        .setName('commands')
-        .setDescription('Manage custom commands.')
-        .addSubcommand((subcommand) =>
-          subcommand
-            .setName('add')
-            .setDescription('Add a new command.')
-            .addStringOption((option) =>
-              option
-                .setName('name')
-                .setRequired(true)
-                .setDescription('The name of the command.'),
-            )
-            .addStringOption((option) =>
-              option
-                .setName('description')
-                .setDescription('The description of the command.')
-                .setRequired(true),
-            )
-            .addStringOption((option) =>
-              option
-                .setName('response')
-                .setRequired(true)
-                .setDescription('The response of the command.'),
-            ),
+const resp = await rest.post(Routes.applicationCommands(config.appId), {
+  body: new SlashCommandBuilder()
+    .setName('commands')
+    .setDescription('Manage custom commands.')
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('add')
+        .setDescription('Add a new command.')
+        .addStringOption((option) =>
+          option
+            .setName('name')
+            .setRequired(true)
+            .setDescription('The name of the command.'),
         )
-        .addSubcommand((subcommand) =>
-          subcommand
-            .setName('remove')
-            .setDescription('Remove a command.')
-            .addStringOption((option) =>
-              option
-                .setName('name')
-                .setRequired(true)
-                .setDescription('The name of the command.'),
-            ),
+        .addStringOption((option) =>
+          option
+            .setName('description')
+            .setDescription('The description of the command.')
+            .setRequired(true),
         )
-        .toJSON(),
-    ],
-  },
-);
+        .addStringOption((option) =>
+          option
+            .setName('response')
+            .setRequired(true)
+            .setDescription('The response of the command.'),
+        ),
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('remove')
+        .setDescription('Remove a command.')
+        .addStringOption((option) =>
+          option
+            .setName('name')
+            .setRequired(true)
+            .setDescription('The name of the command.'),
+        ),
+    )
+    .toJSON(),
+});
+
+console.log(resp);
