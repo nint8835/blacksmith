df67006ed26b1d15afd00351319ef03ac4e46324
Riley Flynn <riley@rileyflynn.me>
2024-10-22 20:25:57 -0230

    Subcommands

--- src/commands/manage.ts
+++ src/commands/manage.ts
@@ -1,7 +1,11 @@
-import { InteractionResponseType, type APIApplicationCommandInteraction } from 'discord-api-types/v10';
+import {
+	InteractionResponseType,
+	type APIApplicationCommandInteraction,
+	type APIChatInputApplicationCommandInteractionData,
+} from 'discord-api-types/v10';
 import { respond } from '../utils';
 
-export async function handleManageCommandsCommand(
+async function handleAddCommand(
 	interaction: APIApplicationCommandInteraction,
 	env: Env,
 	ctx: ExecutionContext,
@@ -9,7 +13,42 @@ export async function handleManageCommandsCommand(
 	return respond({
 		type: InteractionResponseType.ChannelMessageWithSource,
 		data: {
-			content: 'TODO - Manage',
+			content: 'TODO - Add',
 		},
 	});
+}
+
+async function handleRemoveCommand(
+	interaction: APIApplicationCommandInteraction,
+	env: Env,
+	ctx: ExecutionContext,
+): Promise<Response> {
+	return respond({
+		type: InteractionResponseType.ChannelMessageWithSource,
+		data: {
+			content: 'TODO - Remove',
+		},
+	});
+}
+
+export async function handleManageCommandsCommand(
+	interaction: APIApplicationCommandInteraction,
+	env: Env,
+	ctx: ExecutionContext,
+): Promise<Response> {
+	const interactionData = interaction.data as APIChatInputApplicationCommandInteractionData;
+
+	switch (interactionData.options![0].name) {
+		case 'add':
+			return handleAddCommand(interaction, env, ctx);
+		case 'remove':
+			return handleRemoveCommand(interaction, env, ctx);
+		default:
+			return respond({
+				type: InteractionResponseType.ChannelMessageWithSource,
+				data: {
+					content: 'Invalid subcommand.',
+				},
+			});
+	}
 }
