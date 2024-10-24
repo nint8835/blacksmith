4e99afc15b734461bb0cffe67737adc9aedae972
Riley Flynn <riley@rileyflynn.me>
2024-10-24 20:04:53 -0230

    Handle saved commands

--- src/commands/saved.ts
+++ src/commands/saved.ts
@@ -1,4 +1,5 @@
 import { InteractionResponseType, type APIApplicationCommandInteraction } from 'discord-api-types/v10';
+import { CommandData } from '../types';
 import { respond } from '../utils';
 
 export async function handleSavedCommand(
@@ -6,10 +7,23 @@ export async function handleSavedCommand(
     env: Env,
     ctx: ExecutionContext,
 ): Promise<Response> {
+    const commandDataStr = await env.COMMANDS.get(`${interaction.guild_id}:${interaction.data.name}`);
+
+    if (!commandDataStr) {
+        return respond({
+            type: InteractionResponseType.ChannelMessageWithSource,
+            data: {
+                content: `Command ${interaction.data.name} does not exist. This should not be able to happen.`,
+            },
+        });
+    }
+
+    const commandData = JSON.parse(commandDataStr) as CommandData;
+
     return respond({
         type: InteractionResponseType.ChannelMessageWithSource,
         data: {
-            content: 'TODO',
+            content: commandData.text,
         },
     });
 }
